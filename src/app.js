c506ceafb56a619941f7acaf6760d4e9323e15b9
Daisuke <mds87@hotmail.com>
2019-06-01 17:46:22 +0200

    added intro html css and text to second section

--- src/app.js
+++ src/app.js
@@ -9,3 +9,17 @@ import '../node_modules/jquery'
 
 // add popper.js
 import '../node_modules/popper.js'
+
+$(document).ready(function(){
+
+    // scroll font
+    $('#hero').on('DOMMouseScroll mousewheel', function(event){
+        var height = $(window).scrollTop();
+        if(event.originalEvent.detail > 0 || event.originalEvent.wheelDeltaY < 0){
+            $('.hero-title-first').css('left', ('-' + (height / 50) + '%'));
+        }
+        else{
+            $('.hero-title-first').css('left', ('-' + (height / 50) + '%'));
+        }
+    })
+});
