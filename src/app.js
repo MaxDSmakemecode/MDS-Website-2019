ce9dc97b9856e608b33c783879aee41909adc168
Felix Schaffernicht <felix@schaffernicht.com>
2019-06-03 22:34:43 +0200

    Add scrollhandler function for window scrolling

--- src/app.js
+++ src/app.js
@@ -10,22 +10,25 @@ import '../node_modules/jquery'
 // add popper.js
 import '../node_modules/popper.js'
 
-$(document).ready(function(){
- 
-    // scroll font
-    // important:                HERE put some more scroll functions like when you scroll by mouse or arrow keys
-    $(window).on('DOMMouseScroll mousewheel keydown', function(event){
-        var scrollTop = $(window).scrollTop(),
-            elementOffset = $('#hero').offset().top,
-            distance = (scrollTop - elementOffset);
-            console.log(distance);
-        $('.hero-title-first').css('transform', 'translate3d(-' + (distance / 200).toFixed(2) + '%,0,0)');
-        if(event.which.keyCode == 40 || event.which.keyCode == 38){
-            $('.hero-title-first').css('transform', 'translate3d(-' + (distance / 200).toFixed(2) + '%,0,0)');
-        }
-        $('.hero-title-second').css('transform', 'translate3d(' + (distance / 200).toFixed(2) + '%,0,0)');
-        if(event.which.keyCode == 40 || event.which.keyCode == 38){
-            $('.hero-title-second').css('transform', 'translate3d(' + (distance / 200).toFixed(2) + '%,0,0)');
-        }
-    });
-});
+import handleScroll from './handleScroll'
+
+$(document).ready(function() {
+  // scroll font
+  // important:                HERE put some more scroll functions like when you scroll by mouse or arrow keys
+  // $(window).on('DOMMouseScroll mousewheel keydown', function(event){
+  //     var scrollTop = $(window).scrollTop(),
+  //         elementOffset = $('#hero').offset().top,
+  //         distance = (scrollTop - elementOffset);
+  //         console.log(distance);
+  //     $('.hero-title-first').css('transform', 'translate3d(-' + (distance / 200).toFixed(2) + '%,0,0)');
+  //     if(event.which.keyCode == 40 || event.which.keyCode == 38){
+  //         $('.hero-title-first').css('transform', 'translate3d(-' + (distance / 200).toFixed(2) + '%,0,0)');
+  //     }
+  //     $('.hero-title-second').css('transform', 'translate3d(' + (distance / 200).toFixed(2) + '%,0,0)');
+  //     if(event.which.keyCode == 40 || event.which.keyCode == 38){
+  //         $('.hero-title-second').css('transform', 'translate3d(' + (distance / 200).toFixed(2) + '%,0,0)');
+  //     }
+  // });
+
+  $(window).on('scroll', handleScroll)
+})
