e03b06ea0e8a2a403e9c9e7e9a30e8ce959d6bd6
Daisuke <mds87@hotmail.com>
2019-06-22 23:50:01 +0200

    mouse following background disappears on scroll

--- src/mouseFollower.js
+++ src/mouseFollower.js
@@ -1,13 +1,24 @@
 export default function mouseFollower(){
     // fade in mouse following image
     setTimeout(function(){
-        $('.mousefollow-bg').fadeIn(500).removeClass('.hide--element');
-    },750);
+        $('.mousefollow-bg').fadeIn(500).removeClass('.hide--element')
+    },750)
 
     // mouse following image
     $('.mousefollow-bg').on('mousemove', function(e){
-        var moveX = (e.pageX * -1 / 15);
-        var moveY = (e.pageY * -1 / 15);
-        $(this).css('background-position', moveX + 'px ' + moveY + 'px');
-    });
+        const moveX = (e.pageX * -1 / 15)
+        const moveY = (e.pageY * -1 / 15)
+        $(this).css('background-position', moveX + 'px ' + moveY + 'px')
+    })
+
+    // fade out on scroll
+    $(window).on('scroll', function(){
+        const height = $(window).scrollTop();
+        if(height > 0){
+            $('.mousefollow-bg').fadeOut(500)
+        }
+        else{
+            $('.mousefollow-bg').fadeIn(500)
+        }
+    })
 }
