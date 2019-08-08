7f917262a2f6abdfed518522b8fcffd53be32fe2
Daisuke <mds87@hotmail.com>
2019-08-08 23:49:16 +0200

    fixed title animation with css keyframes and es6

--- src/onLoadEvents.js
+++ src/onLoadEvents.js
@@ -1,33 +1,7 @@
 export default function onLoadEvents(){
     // slogan text animation on front page
     const sloganAnimation = document.querySelector('.hero-title-first');
-
-    setTimeout(function(){
-      setTranslate(15, sloganAnimation);
-    }, 500);
-
-    function setTranslate(xPos, el){
-      el.style.transform = 'translate3d(' + xPos + '%, 0, 0);';
-    }
-
-    // sloganAnimation('.hero-title-first', 250);
-    // sloganAnimation('.hero-title-second', 500);
-
-    // function sloganAnimation(target, target_delay){
-    //   setTimeout(function(){
-    //     $(target).animate({
-    //       'opacity': '1'
-    //     },{
-    //       step: function(now){
-    //         $(this).css({'transform': 'translate3d(' + now + '%,0,0)'});
-    //       },
-    //       duration: 150,
-    //       easing: 'swing',
-    //       queue: false,
-    //       complete: function(){
-    //         console.log('animation done');
-    //       }
-    //     })
-    //   }, target_delay);
-    // }
+    const sloganAnimationSecond = document.querySelector('.hero-title-second');
+    sloganAnimation.classList.add('title-first--animation');
+    sloganAnimationSecond.classList.add('title-second--animation');
 }
