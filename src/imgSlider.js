b42797da24e37b27aaf4a88cd032589cbd70efdf
Daisuke <mds87@hotmail.com>
2019-09-14 20:56:14 +0200

    tried to create clickable slider

--- src/imgSlider.js
+++ src/imgSlider.js
@@ -1,36 +1,6 @@
 export default function imgSlider(){
+    const portfolioSliderContainer = document.getElementById('port-image-slider__carousel');
 
-    // duration of how long a slide stays static
-    const slideInterval = 4000;
-
-    // get all 'figure' elements as an array
-    function getFigures(){
-        return document.getElementById('port-image__carousel').getElementsByTagName('figure');
-    }
-
-    // change the slides with css classes
-    function moveForward(){
-        let pointer = 0;
-        let figures = getFigures();
-
-        for(var i = 0; i < figures.length; i++){
-            if(figures[i].className == 'visible-first' || figures[i].className == 'visible'){
-                figures[i].className = 'hidden';
-                pointer = i;
-            }
-        }
-        if(++pointer == figures.length){
-            pointer = 0;
-        }
-        figures[pointer].className = 'visible';
-        setTimeout(moveForward, slideInterval);
-    }
-    
-    // function for the slide animation with the selected slide interval
-    function startPlayback(){
-        setTimeout(moveForward, slideInterval);
-    }
-
-    // call the slide animation
-    startPlayback();
+    // READ THIS #################################################
+    // do the multiple slider from stackoverflow 
 }
--- src/scrollHandler.js
+++ src/scrollHandler.js
@@ -1,21 +1,21 @@
 export default function scrollHandler(){
-    // slide in elements when scrolled to a particular point
+    // slide in elements when scrolled to a particular point 
 
     // debounce function to reduce function call amount over scroll
-    // function debounce(func, wait = 20, immediate = true){
-    //     let timeout;
-    //     return function(){
-    //         let context = this, args = arguments;
-    //         let later = function(){
-    //             timeout = null;
-    //             if(!immediate)func.apply(context, args);
-    //         };
-    //         let callNow = immediate && !timeout;
-    //         clearTimeout(timeout);
-    //         timeout = setTimeout(later, wait);
-    //         if(callNow)func.apply(context, args);
-    //     };
-    // };
+    function debounce(func, wait = 20, immediate = true){
+        let timeout;
+        return function(){
+            let context = this, args = arguments;
+            let later = function(){
+                timeout = null;
+                if(!immediate)func.apply(context, args);
+            };
+            let callNow = immediate && !timeout;
+            clearTimeout(timeout);
+            timeout = setTimeout(later, wait);
+            if(callNow)func.apply(context, args);
+        };
+    };
 
     // grab all slides
     const imagesForFadeIn = document.querySelectorAll('.port-slide__in');
@@ -43,5 +43,5 @@ export default function scrollHandler(){
 
     // window event scroll
     // change the wait amount to customize amount of function calls in 'debounce(checkSlide, 500)' for example to have a function call every half second 
-    window.addEventListener('scroll', checkSlide);
+    window.addEventListener('scroll', debounce(checkSlide));
 }
