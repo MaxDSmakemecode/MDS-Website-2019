2e69cea15636fe3bdb5042a8acb2c44d9b41d993
Daisuke <mds87@hotmail.com>
2020-05-23 21:26:50 +0200

    fixed full size navi so no scrolling and safari bugs with position fixed

--- src/burgerNav.js
+++ src/burgerNav.js
@@ -1,6 +1,7 @@
 export default function burgerNav(){
     const navBar = document.querySelector('.navbar');
     const mdsNavbar = document.querySelector('#mds-navbar');
+    const mdsBurger = document.querySelector('#mds-burger');
     const mdsMenuLinks = document.querySelectorAll('.mds-menu-link');
 
     // add black background to navigation
@@ -14,6 +15,13 @@ export default function burgerNav(){
             }
             else if(!navBar.classList.contains('navbar-expand-lg') && window.innerWidth > '992'){
                 mdsNavbar.classList.remove('mds-no--before');
+            }
+
+            if(mdsBurger.classList.contains('collapsed') && window.pageYOffset >= 100 || mdsBurger.classList.contains('collapsed') && window.innerWidth <= '992'){
+                document.body.style.overflow = 'hidden';
+            }
+            else {
+                document.body.style.overflow = 'auto';
             }
         })
     })
